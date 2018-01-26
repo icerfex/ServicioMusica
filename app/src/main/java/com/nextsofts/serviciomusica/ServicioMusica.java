672fed0ec41baf4927d4076d362afa18f8829fe6
icerfex <arielalcocericerfex>
2018-01-26 17:32:29 -0400

    creacion de una notificacion

--- app/src/main/java/com/nextsofts/serviciomusica/ServicioMusica.java
+++ app/src/main/java/com/nextsofts/serviciomusica/ServicioMusica.java
@@ -1,9 +1,13 @@
 package com.nextsofts.serviciomusica;
 
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
 
 /**
@@ -12,6 +16,7 @@ import android.widget.Toast;
 public class ServicioMusica extends Service {
 
     MediaPlayer reproductor;
+    private static final int ID_NOTIFICACION_CREAR=1;
 
     @Override
     public void onCreate(){
@@ -21,6 +26,14 @@ public class ServicioMusica extends Service {
 
     @Override
     public int onStartCommand(Intent intenc,int flags,int idArranque){
+        NotificationCompat.Builder notific=new NotificationCompat.Builder(this)
+                .setContentTitle("creando servicio de musica")
+                .setSmallIcon(R.drawable.image)
+                .setContentText("pulsa en el boton para ver");
+        NotificationManager notificationManaget= (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManaget.notify(ID_NOTIFICACION_CREAR,notific.build());
+
+
         Toast.makeText(this,"servicio arrancado"+idArranque,Toast.LENGTH_SHORT).show();
         reproductor.start();
         return START_STICKY;
